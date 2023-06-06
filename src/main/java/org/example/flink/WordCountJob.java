d7e6016e893986d9403b450f735a89cd2b329ac9
xuyongshi <heliocc@163.com>
2023-06-06 23:51:58 +0800

    add test

--- src/main/java/org/example/flink/WordCountJob.java
+++ src/main/java/org/example/flink/WordCountJob.java
@@ -1,14 +1,25 @@
 package org.example.flink;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
+import java.util.Arrays;
 
 public class WordCountJob {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
+        DataSource<String> dataSource = env.fromCollection(Arrays.asList("Hello", "To be,or not to be:that is the question:"));
 
+        dataSource.map(new MapFunction<String, Integer>() {
+            @Override
+            public Integer map(String s) throws Exception {
+                return s.length();
+            }
+        }).print();
     }
 
 }
---
+++ src/test/java/org/example/flink/WordCountStreamTests.java
@@ -0,0 +1,12 @@
+package org.example.flink;
+
+import org.junit.Test;
+
+public class WordCountStreamTests {
+
+    @Test
+    public void test(){
+        System.out.println(123);
+    }
+
+}
