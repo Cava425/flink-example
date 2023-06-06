cd210a51e60312424e6fc56c96261892254200fb
xuyongshi <heliocc@163.com>
2023-06-06 23:23:44 +0800

    first word count

---
+++ src/main/java/org/example/flink/WordCountJob.java
@@ -0,0 +1,14 @@
+package org.example.flink;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+public class WordCountJob {
+
+    public static void main(String[] args) {
+
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
+
+}
---
+++ src/main/java/org/example/flink/WordCountStreamJob.java
@@ -0,0 +1,49 @@
+package org.example.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class WordCountStreamJob {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        /**
+         * 打开终端，运行 nc -lp 9999
+         *
+         * 向集群提交作业
+         * 1. mvn clean package
+         * 2.使用flink客户端工具提交作业
+         * $ bin/flink run --class org.example.flink.WordCountStreamJob D:/AppData/Projects/GitHub/flink-example/target/flink-example-1.0-SNAPSHOT.jar
+         */
+        DataStreamSource<String> streamSource = env.socketTextStream("localhost", 9999);
+
+        DataStream<Tuple2<String, Integer>> wordCount = streamSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] tokens = line.split("\\s");
+                for (String token : tokens) {
+                    if (token.length() > 0) {
+                        collector.collect(new Tuple2<>(token, 1));
+                    }
+                }
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+
+        // Sink
+        wordCount.print();
+
+        env.execute("Word Count Job");
+    }
+
+}
